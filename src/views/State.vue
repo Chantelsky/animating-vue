113b75f345379430601a2392f5e4b7d05d777225
Chantelsky <chantel.sky@outlook.com>
2020-08-30 01:38:44 +1000

    tweening with gsap in vue

---
+++ src/views/State.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div :style="{ width: tweenedNumber + 'px' }" class="bar">
+      <span> {{ tweenedNumber.toFixed(0) }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import gsap from 'gsap'
+export default {
+  data() {
+    return {
+      number: 0,
+      tweenedNumber: 0
+    }
+  },
+  watch: {
+    number(newValue) {
+      //animate our data
+      gsap.to(this.$data, {
+        duration: 1,
+        ease: 'circ.out',
+        tweenedNumber: newValue
+      })
+    }
+  },
+  methods: {
+    randomNumber() {
+      this.number = Math.floor(Math.random() * (800 - 0))
+    }
+  },
+  created() {
+    setInterval(this.randomNumber, 1500)
+  }
+}
+</script>
+
+<style>
+.bar {
+  background: #42b983;
+  color: #fff;
+}
+</style>
