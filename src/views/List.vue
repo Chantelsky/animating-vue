551317b4648a3d8a5a0850ca6a554d90e9a7c3fe
Chantelsky <chantel.sky@outlook.com>
2020-08-29 16:44:34 +1000

    group transitions

---
+++ src/views/List.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div class="input">
+      <input type="text" v-model="newContact" placeholder="Name" />
+      <button @click="addContact">Add Contact</button>
+      <button @click="sortContacts">Sort Contacts</button>
+    </div>
+
+    <transition-group name="slide-up" tag="ul">
+      <li v-for="contact in contacts" :key="contact">{{ contact }}</li>
+    </transition-group>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newContact: '',
+      contacts: ['Beau Thabeast', 'Cindy Rella', 'Alice Vunderlind']
+    }
+  },
+  methods: {
+    addContact() {
+      this.contacts.push(this.newContact)
+      this.newContact = ''
+    },
+    sortContacts() {
+      this.contacts.sort()
+    }
+  }
+}
+</script>
+
+<style>
+.input {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+input {
+  width: 20%;
+  height: 20px;
+}
+
+button {
+  width: 120px;
+}
+
+.slide-up-enter {
+  opacity: 0;
+  transform: translateY(10px);
+}
+
+.slide-up-enter-active {
+  transition: all 0.2s ease;
+}
+
+.slide-up-move {
+  transition: transform 0.5s ease-out;
+}
+</style>
